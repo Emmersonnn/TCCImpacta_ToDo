999e2202a4e4596914fdc34caf55c57381712309
Leoimp2024 <leonardo.paixao@aluno.faculdadeimpacta.com.br>
2024-10-10 15:47:48 -0300

    home

--- tccimpacta_todo.client/src/app/tenant/views/home/home.component.ts
+++ tccimpacta_todo.client/src/app/tenant/views/home/home.component.ts
@@ -8,3 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class HomeComponent { }
+
+
+<script>
+  function addItem() {
+    const newItem = document.getElementById('new-item').value;
+    const quantity = document.getElementById('quantity').value;
+
+    if (newItem && quantity) {
+      const list = document.getElementById('shopping-list');
+      const li = document.createElement('li');
+      li.innerHTML = `<span class="item-name">${newItem}</span><span class="item-quantity">${quantity}</span><button class="remove-btn" onclick="removeItem(this)">Remover</button>`;
+      list.appendChild(li);
+
+      document.getElementById('new-item').value = '';
+      document.getElementById('quantity').value = '';
+    }
+  }
+
+function removeItem(button) {
+  button.parentElement.remove();
+}
+</script>
